a25cdb1d9b08dcbcf7d38939069a0a912d522c2c
alexlueng <alexlueng@icloud.com>
2022-03-06 22:25:11 +0800

    finished lottery coding step

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ brownie networks add development mainnet-fork cmd=ganache-cli host=http://127.0.
 
 https://eth-mainnet.alchemyapi.io/v2/L9mpEMRyQCQTHSJ_Ycrg5qY0QC3uts3t
 https://eth-mainnet.alchemyapi.io/v2/L9mpEMRyQCQTHSJ_Ycrg5qY0QC3uts3t
+
+
+test smart contract is a very import step,
+you should test every line in the smart contract
+
+What should I do now?
+- figure out so many questions
+- test文件夹下的sol文件如何编译，不同版本的sol文件
+- 测试网络与本地网络的区别，分别是如何部署的
+- brownie account
+- vrf1 与 vrf2
+  
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,12 +3,20 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+// since vrf1 is deprete, we should change it to vrf2
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
 
     address payable[] public players;
     uint256 public usdEntryFee;
+    address payable public recentWinner;
+    uint256 public randomness;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    uint256 public fee;
+    bytes32 public keyHash;
+    event RequestedRandomness(bytes32 requestId);
+
     
     enum LOTTERY_STATE {
         OPEN,
@@ -17,13 +25,23 @@ contract Lottery is Ownable {
     }
     LOTTERY_STATE public lottery_state;
 
-    constructor(address _priceFeedAddress) public {
+    constructor(address _priceFeedAddress,
+                address _vrfCoordinator,
+                address _link,
+                uint256 _fee,
+                bytes32 _keyHash) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyHash = _keyHash;
     }
 
-    function enter() public {
+
+// TypeError: "msg.value" and "callvalue()" can only be used in payable public functions.
+//  Make the function "payable" or use an internal function to avoid this error.
+
+    function enter() payable public {
         // $50 minimum
         // because in 0.8 msg.sender is not automatically payable anymore.
         require(lottery_state == LOTTERY_STATE.OPEN);
@@ -57,15 +75,31 @@ contract Lottery is Ownable {
         // randomness
         // get a truly random number in a determine system is problary possible
         // since all the params can be preditable, this is an unacceptable way to do in our real app
-        unit256(
-            keccak256(
-                abi.encodePacked(
-                    nonce,
-                    msg.sender,
-                    block.difficulty,
-                    block.timestamp
-                )
-            )
-        ) % players.length;
+        // unit256(
+        //     keccak256(
+        //         abi.encodePacked(
+        //             nonce,
+        //             msg.sender,
+        //             block.difficulty,
+        //             block.timestamp
+        //         )
+        //     )
+        // ) % players.length;
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyHash, fee);
+        emit RequestedRandomness(requestId);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
+        require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER, "You aren't there yet");
+        require(_randomness > 0, "random-not-found");
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        
+        // reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        _randomness = _randomness;
     }
 }
